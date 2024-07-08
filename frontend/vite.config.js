f4b531d2c30a442e0300e075c5d07f72aea365a3
Tanmayyy209 <tanmaypatil4518@gmail.com>
2024-07-08 16:22:11 +0530

    change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,  // This makes the server listen on all network interfaces
+    port: ,  // Optionally, specify a port
+  }
 })
